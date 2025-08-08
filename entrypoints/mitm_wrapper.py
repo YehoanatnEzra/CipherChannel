8b19de682fcd33d8d573dc55aaa60a79211dfe9d
Yehonatan Ezra <yonzra12@gmail.com>
2025-08-08 19:08:41 +0300

    Add files via upload

---
+++ entrypoints/mitm_wrapper.py
@@ -0,0 +1,156 @@
+# mitm_wrapper.py
+import socket, time
+import random
+
+
+class AttackException(Exception):
+    pass
+
+
+# Addresses and ports for the MITM
+LISTEN_IP = "127.0.0.1"
+LISTEN_PORT = 65431
+FORWARD_IP = "127.0.0.1"
+FORWARD_PORT = 65432
+
+HEADER = "\033[95m"
+OKBLUE = "\033[94m"
+OKCYAN = "\033[96m"
+OKGREEN = "\033[92m"
+WARNING = "\033[93m"
+FAIL = "\033[91m"
+ENDC = "\033[0m"
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+
+# Buffer size for receiving data
+BUFFER_SIZE = 4096
+DELAY = 0.5  # 0.5-second delay
+
+message_number = 0
+messages_client_to_server = []
+messages_server_to_client = []
+
+
+def attack_payload(payload: str, changes: int = 5) -> str:
+    chars = list(payload)
+    for _ in range(changes):
+        index = random.randint(0, len(chars))
+        chars[index % len(chars)] = random.choice("abcde1234567890")
+    return "".join(chars)
+
+
+# Will not be strictly a predefined list, can be a random function
+DROP_CLIENT_TO_SERVER = {3, 7, 11, 12}
+CHANGE_CLIENT_TO_SERVER = {2, 6, 10}
+DROP_SERVER_TO_CLIENT = {1, 5, 8}
+CHANGE_SERVER_TO_CLIENT = {4, 9}
+
+
+def handle_client(client_socket):
+    global message_number
+    forward_socket = None
+    try:
+        # Receive data from the client
+        client_data = client_socket.recv(BUFFER_SIZE)
+        messages_client_to_server.append(client_data)
+        if not client_data:
+            print(f"{OKGREEN}[-][{time.ctime()}] No data received from client.{ENDC}")
+            return
+
+        print(f"{OKGREEN}[>][{time.ctime()}] Received from client:{ENDC} {client_data}")
+
+        ### DROP PACKET ###
+        if message_number and message_number in DROP_CLIENT_TO_SERVER:
+            print(
+                f"{WARNING}[X][{time.ctime()}] Dropping, returning empty string to client:{ENDC}"
+            )
+            client_socket.sendall(bytes())
+            raise AttackException("Drop client to server")
+
+        ### ATTACK PAYLOAD ###
+        if message_number and message_number in CHANGE_CLIENT_TO_SERVER:
+            client_data = attack_payload(client_data.decode("utf-8")).encode("utf-8")
+            print(
+                f"{WARNING}[X][{time.ctime()}] Modifying payload to:{ENDC} {client_data}"
+            )
+
+        # Forward the data to the target server
+        forward_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        forward_socket.connect((FORWARD_IP, FORWARD_PORT))
+
+        ### REPLAY A PREVIOUS ###
+        REPLAY = random.randint(1, 8)
+        if message_number and REPLAY < len(messages_client_to_server):
+            forward_socket.sendall(messages_client_to_server[REPLAY])
+            forward_socket.recv(BUFFER_SIZE)
+
+        forward_socket.sendall(client_data)
+
+        # Receive response from the target server
+        server_response = forward_socket.recv(BUFFER_SIZE)
+        messages_server_to_client.append(server_response)
+        print(
+            f"{OKBLUE}[<][{time.ctime()}] Received from server:{ENDC} {server_response}"
+        )
+
+        ### ATTACK RESPONSE ###
+        if message_number and message_number in CHANGE_SERVER_TO_CLIENT:
+            server_response = attack_payload(server_response.decode("utf-8")).encode(
+                "utf-8"
+            )
+            print(
+                f"{WARNING}[X][{time.ctime()}] Modifying payload to:{ENDC} {server_response}"
+            )
+
+        ### DROP RESPONSE ###
+        if message_number and message_number in DROP_SERVER_TO_CLIENT:
+            server_response = bytes()
+            print(
+                f"{WARNING}[X][{time.ctime()}] Modifying payload to:{ENDC} {server_response}"
+            )
+
+        # Send the response back to the client
+        client_socket.sendall(server_response)
+
+    except AttackException as e:
+        print(f"[-][{time.ctime()}] MITM Attack: {e}")
+
+    except Exception as e:
+        print(f"[-][{time.ctime()}] Error occurred while handling client: {e}")
+    finally:
+        message_number += 1
+
+        # Close the forward and client sockets
+
+        if forward_socket:
+            forward_socket.close()
+        client_socket.close()
+
+
+def mitm_proxy():
+    try:
+        # Create a listening socket for incoming connections
+        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listen_socket.bind((LISTEN_IP, LISTEN_PORT))
+        listen_socket.listen(5)
+        print(f"[*][{time.ctime()}] Listening on {LISTEN_IP}:{LISTEN_PORT}")
+
+        while True:
+            # Accept a new connection from a client
+            client_socket, client_addr = listen_socket.accept()
+            print(
+                f"{HEADER}[+][{time.ctime()}] Accepted connection from {client_addr}{ENDC}"
+            )
+
+            # Handle the client in a separate function
+            handle_client(client_socket)
+
+    except Exception as e:
+        print(f"[-][{time.ctime()}] Error occurred in proxy: {e}")
+    finally:
+        listen_socket.close()
+
+
+if __name__ == "__main__":
+    mitm_proxy()
---
+++ vpn/crypto.py
@@ -0,0 +1,181 @@
+from json import JSONDecodeError
+from Crypto.PublicKey import ECC
+from Crypto.Hash import SHAKE128, SHA256, HMAC
+from Crypto.Protocol.DH import key_agreement
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from base64 import b64encode, b64decode
+import json
+
+
+def is_integrity_error(e: Exception) -> bool:
+    """ Checks if the exception is an integrity error. """
+    return isinstance(e, IntegrityError) or isinstance(e, JSONDecodeError)
+
+
+def is_general_error(e: Exception) -> bool:
+    """ Checks if the exception is an general error. """
+    return isinstance(e, InvalidNonceError) or isinstance(e, DroppedMessageError)
+
+
+class IntegrityError(Exception):
+    """
+        Raised when a message fails integrity checks.
+        Covers HMAC mismatches or invalid JSON payloads.
+        """
+    def __init__(self, msg="Message does not pass integrity checks"):
+        self.message = msg
+        super().__init__(self.message)
+
+
+class InvalidHashError(IntegrityError):
+    """
+       Raised when HMAC verification fails.
+       Inherits from IntegrityError.
+       """
+    def __init__(self, hmac: str):
+        message = f"Invalid HMAC provided: {hmac}"
+        super().__init__(message)
+
+
+class InvalidNonceError(Exception):
+    """
+       Raised when the received nonce does not match the expected value.
+       Helps prevent replay attacks.
+       """
+    def __init__(self, nonce: int, expected_nonce):
+        message = f"Invalid nonce provided: {nonce}. Expected: {expected_nonce}"
+        self.nonce = nonce
+        super().__init__(message)
+
+
+class DroppedMessageError(Exception):
+    """
+       Raised when an empty acknowledgment is received,
+       indicating that a message was dropped.
+       """
+    def __init__(self):
+        message = "Empty acks are drops messages"
+        super().__init__(message)
+
+
+def kdf(x):
+    """
+    Key derivation function used for DH secret generation
+
+    Consider: for more security, this could take in some context of the operation (maybe a nonce?)
+    More info: https://www.pycryptodome.org/src/protocol/dh
+    """
+    return SHAKE128.new(x).read(32)
+
+
+def generate_keypair() -> ECC.EccKey:
+    """
+    Randomly generates a 256-bit ECC keypair
+    https://en.wikipedia.org/wiki/Elliptic-curve_cryptography
+    """
+    return ECC.generate(curve="p256")
+
+
+def generate_shared_secret(private_key: ECC.EccKey, public_key: ECC.EccKey) -> bytes:
+    """
+    Uses DH to generate a byte array representing a shared secret given an ECC keypair
+    """
+    return key_agreement(static_priv=private_key, static_pub=public_key, kdf=kdf)
+
+
+def derive_aes256_key(secret: bytes, salt: bytes) -> bytes:
+    """Generates a 256-bit key intended for AES-CBC encryption, derived from a secret and a salt"""
+    return SHA256.new(secret + salt).digest()
+
+
+def derive_auth_key(secret: bytes) -> bytes:
+    """Generates an authentication key derived from a secret, according to our protocol"""
+    return derive_aes256_key(secret, "auth".encode("utf-8"))
+
+
+def derive_enc_key(secret: bytes) -> bytes:
+    """Generates an encryption key derived from a secret, according to our protocol"""
+    return derive_aes256_key(secret, "enc".encode("utf-8"))
+
+
+def serialize_key(key: ECC.EccKey) -> tuple[str, str]:
+    """Generates a base64-encoded DER encoding of an ECC key in a tuple (private_key, public_key)"""
+    priv_der_bytes, pub_der_bytes = (
+        key.export_key(format="DER"),
+        key.public_key().export_key(format="DER"),)
+
+    return b64encode(priv_der_bytes).decode("utf-8"), b64encode(pub_der_bytes).decode("utf-8")
+
+
+def parse_key(der_b64: str) -> ECC.EccKey:
+    """Loads an ECC key (private or public) into a pycrpto EccKey object from its base64-encoded DER format"""
+    try:
+        key = ECC.import_key(b64decode(der_b64))
+    except ValueError:  # key got modified, it's Invalid
+        raise IntegrityError()
+    return key
+
+
+def dict_to_jsonb(d: dict) -> bytes:
+    def recursive_sets_to_lists(x):
+        if isinstance(x, dict):
+            return {key: recursive_sets_to_lists(value) for key, value in x.items()}
+        elif isinstance(x, set):
+            return list(x)
+        elif isinstance(x, list):
+            return [recursive_sets_to_lists(elem) for elem in x]
+        else:
+            return x
+
+    return json.dumps(recursive_sets_to_lists(d)).encode("utf-8")
+
+
+def encrypt(plaintext: str, enc_key: bytes) -> tuple:
+    cipher = AES.new(enc_key, AES.MODE_CBC)
+    iv = cipher.iv.hex()
+    ciphertext = cipher.encrypt(pad(plaintext.encode("utf-8"), AES.block_size)).hex()
+    return iv, ciphertext
+
+
+def deserialize_payload(ciphertext: str) -> dict:
+    return json.loads(ciphertext)
+
+
+def validate_payload_hmac(payload: dict, auth_key: bytes) -> bool:
+    """Validates that the body and MAC match"""
+    hmac = HMAC.new(auth_key, dict_to_jsonb(payload["body"]), digestmod=SHA256)
+
+    try:
+        hmac.hexverify(payload["hmac"])
+        return True
+    except ValueError:
+        # Could not verify HMAC
+        raise InvalidHashError(payload["hmac"])
+    except Exception:
+        # unhandled exception
+        raise
+
+
+def parse_payload(payload: dict) -> tuple[bytes, bytes, int, ECC.EccKey]:
+    """Returns a tuple with (ciphertext, iv, nonce, new_pub_key)"""
+
+    body = payload["body"]
+
+    ciphertext = bytes.fromhex(body["text"])
+    iv = bytes.fromhex(body["iv"])
+    nonce = body["nonce"]
+    new_pub_key = parse_key(body["new_pub_key"])
+
+    return ciphertext, iv, nonce, new_pub_key
+
+
+def decrypt(ciphertext: bytes, iv: bytes, enc_key: bytes) -> str:
+    cipher = AES.new(enc_key, AES.MODE_CBC, iv=iv)
+
+    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return plaintext.decode("utf-8")
+
+
+def compute_hmac(auth_key: bytes, msg_body: bytes):
+    return HMAC.new(auth_key, msg_body, digestmod=SHA256)
